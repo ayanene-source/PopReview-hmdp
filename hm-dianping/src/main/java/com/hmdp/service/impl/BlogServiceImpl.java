8d9567265ead9cf170082954074eb61d7eac2695
odoo <2533831848@qq.com>
2026-02-04 23:45:26 +0800

    Follow and information push

--- hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,12 +1,13 @@
 package com.hmdp.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.BooleanUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.Result;
+import com.hmdp.dto.ScrollResult;
 import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Blog;
+import com.hmdp.entity.Follow;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
@@ -15,12 +16,17 @@ import com.hmdp.service.IUserService;
 import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
+
+import static com.hmdp.utils.RedisConstants.FEED_KEY;
 
 /**
  * <p>
@@ -37,6 +43,8 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
     private IUserService userService;
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private FollowServiceImpl followService;
     @Override
     public Result queryHotBlog(Integer current) {
         // 根据用户查询
@@ -48,7 +56,7 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         // 查询用户
         records.forEach(blog ->{
            this.queryBlogUser(blog);//查询blog用户
-           this.isBolgLiked(blog);//查询blog是否被点赞
+           this.isBlogLiked(blog);//查询blog是否被点赞
         });
         return Result.ok(records);
     }
@@ -63,11 +71,11 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
         //2. 查询blog的用户
         queryBlogUser(blog);
         //3.查询blog是否被点赞
-        isBolgLiked(blog);
+        isBlogLiked(blog);
         return Result.ok(blog);
     }
 
-    private void isBolgLiked(Blog blog) {
+    private void isBlogLiked(Blog blog) {
         //1/获取登录用户
         UserDTO user = UserHolder.getUser();
         if (user == null){
@@ -129,6 +137,79 @@ public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IB
                 .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
                 .collect(Collectors.toList());
         return Result.ok(userDTOS);
+    }
+
+    @Override
+    public Result saveBlog(Blog blog) {
+        //1获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        blog.setUserId(userId);
+        //2保存探店博客
+        boolean isSuccess = save(blog);
+        if(!isSuccess){
+            return Result.fail("发布笔记失败");
+        }
+        //3查询笔记作者userId的所有粉丝 select * from tb_follow where follow_user_id = ?
+        List<Follow> follows = followService.query().eq("follow_user_id", userId).list();
+        //4推送笔记id给所有粉丝
+
+        for (Follow follow : follows){
+            //4.1获取粉丝id
+            Long userId1 = follow.getUserId();
+            //4.2推送
+            String key = "feed:" + userId1;
+            stringRedisTemplate.opsForZSet().add(key,blog.getId().toString(),System.currentTimeMillis());
+        }
+        //返回id
+        return Result.ok(blog.getId());
+
+    }
+
+    @Override
+    public Result queryBlogOfFollow(Long max, Integer offset) {
+        //1. 获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        //2. 查询该用户收件箱（之前我们存的key是固定前缀 + 粉丝id），所以根据当前用户id就可以查询是否有关注的人发了笔记
+        String key = FEED_KEY + userId;
+        Set<ZSetOperations.TypedTuple<String>> typeTuples = stringRedisTemplate.opsForZSet()
+                .reverseRangeByScoreWithScores(key, 0, max, offset, 2);
+        //3. 非空判断
+        if (typeTuples == null || typeTuples.isEmpty()){
+            return Result.ok(Collections.emptyList());
+        }
+        //4. 解析数据，blogId、minTime（时间戳）、offset，这里指定创建的list大小，可以略微提高效率，因为我们知道这个list就得是这么大
+        ArrayList<Long> ids = new ArrayList<>(typeTuples.size());
+        long minTime = 0;
+        int os = 1;
+        for (ZSetOperations.TypedTuple<String> typeTuple : typeTuples) {
+            //4.1 获取id
+            String id = typeTuple.getValue();
+            ids.add(Long.valueOf(id));
+            //4.2 获取score（时间戳）
+            long time = typeTuple.getScore().longValue();
+            if (time == minTime){
+                os++;
+            }else {
+                minTime = time;
+                os = 1;
+            }
+        }
+        //解决SQL的in不能排序问题，手动指定排序为传入的ids
+        String idsStr = StrUtil.join(",", ids);
+        //5. 根据id查询blog
+        List<Blog> blogs = query().in("id", ids).last("ORDER BY FIELD(id," + idsStr + ")").list();
+        for (Blog blog : blogs) {
+            //5.1 查询发布该blog的用户信息
+            queryBlogUser(blog);
+            //5.2 查询当前用户是否给该blog点过赞
+            isBlogLiked(blog);
+        }
+        //6. 封装结果并返回
+        ScrollResult scrollResult = new ScrollResult();
+        scrollResult.setList(blogs);
+        scrollResult.setOffset(os);
+        scrollResult.setMinTime(minTime);
+        return Result.ok(scrollResult);
     }
 
     private void queryBlogUser(Blog blog) {
--- hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
+++ hm-dianping/src/main/java/com/hmdp/service/impl/FollowServiceImpl.java
@@ -1,10 +1,23 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.hmdp.dto.Result;
+import com.hmdp.dto.UserDTO;
 import com.hmdp.entity.Follow;
 import com.hmdp.mapper.FollowMapper;
 import com.hmdp.service.IFollowService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.UserHolder;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * <p>
@@ -16,5 +29,69 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class FollowServiceImpl extends ServiceImpl<FollowMapper, Follow> implements IFollowService {
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+    @Resource
+    private IUserService userService;
 
+    @Override
+    public Result follow(Long followUserId, Boolean isFollow) {
+        // 1.获取登录用户
+        Long userId = UserHolder.getUser().getId();
+        // 2.判断是否关注
+        if (isFollow) {
+            // 关注，新增数据
+            Follow follow = new Follow();
+            follow.setUserId(userId);
+            follow.setFollowUserId(followUserId);
+            boolean isSuccess = save(follow);
+
+            if (isSuccess) {
+                // 保存关注用户的id到redis的set集合
+                stringRedisTemplate.opsForSet().add("follows:" + userId, followUserId.toString());
+
+            }
+        } else {
+            // 取消关注，删除数据
+          boolean isSuccess =  remove(new QueryWrapper<Follow>()
+                    .eq("user_id", userId)
+                    .eq("follow_user_id", followUserId));
+          if (isSuccess) {
+              // 删除关注用户的id从Redis的set集合中
+              stringRedisTemplate.opsForSet().remove("follows:" + userId, followUserId.toString());
+          }
+        }
+
+        return Result.ok("操作成功");
+    }
+
+    @Override
+    public Result isFollow(Long followUserId) {
+        Long userId = UserHolder.getUser().getId();
+        Integer count = query().eq("user_id", userId).eq("follow_user_id", followUserId).count();
+        return Result.ok(count > 0);
+
+    }
+
+    @Override
+    public Result followCommons(Long id) {
+        // 获取当前用户
+        Long userId = UserHolder.getUser().getId();
+        // 求交集
+        String key = "follows:" + id;
+        String key2 = "follows:" + userId;
+        Set<String> intersect = stringRedisTemplate.opsForSet().intersect(key, key2);
+        // 判断结果是否为空
+        if (intersect == null || intersect.isEmpty()) {
+            return Result.ok(Collections.emptyList());
+        }
+        //解析id集合
+        List<Long> ids = intersect.stream().map(Long::valueOf).collect(Collectors.toList());
+        //查询用户
+        List<UserDTO> users = userService.listByIds(ids)
+                .stream()
+                .map(user -> BeanUtil.copyProperties(user, UserDTO.class))
+                .collect(Collectors.toList());
+        return Result.ok(users);
+    }
 }
